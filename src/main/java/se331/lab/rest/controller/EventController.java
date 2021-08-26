617c9600cfd430f06afdfd40a869bd234ea13901
Oralita <oat431@gmail.com>
2021-08-26 21:31:12 +0700

    [🌵 finish] : the number 3.4

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,10 +1,13 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 
 import javax.annotation.PostConstruct;
@@ -103,10 +106,29 @@ public class EventController {
 
         Integer firstIndex = (page-1) * perPage;
         List<Event> output = new ArrayList<>();
-
-        for(int i=firstIndex; i<firstIndex + perPage ; i++){
-            output.add(eventList.get(i));
+        try {
+            for(int i=firstIndex; i<firstIndex + perPage ; i++){
+                output.add(eventList.get(i));
+            }
+        }catch (IndexOutOfBoundsException ex){
+            return ResponseEntity.ok(output);
         }
         return ResponseEntity.ok(output);
+    }
+
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
+        Event output = null;
+        for (Event event : eventList) {
+            if(event.getId().equals(id)){
+                output = event;
+                break;
+            }
+        }
+        if(output != null){
+            return ResponseEntity.ok(output);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
+        }
     }
 }
